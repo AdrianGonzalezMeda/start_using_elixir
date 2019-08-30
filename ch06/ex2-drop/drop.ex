0330cda61cae9129b69c84c8f4d2f686c61ea03e
Adrian Gonzalez <adriangonzalezmeda@gmail.com>
2019-08-30 20:21:17 +0200

    Chapter 6

---
+++ ch06/ex2-drop/drop.ex
@@ -0,0 +1,11 @@
+defmodule ListDrop do
+  def falls(list) do
+    falls(list, [])
+  end
+  def falls([], results) do
+    results
+  end
+  def falls([head|tail], results) do
+    falls(tail, [Drop.fall_velocity(head) | results])
+  end
+end
